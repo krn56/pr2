8c85223df0dfc4167aa28ecdb43943ea449aaa1b
krn56 <karanpatelkrn@gmail.com>
2018-01-31 21:43:19 -0500

    IKTrajectory class half

--- ik_trajectory/src/ik_trajectory.cpp
+++ ik_trajectory/src/ik_trajectory.cpp
@@ -16,7 +16,35 @@ static const std::string ARM_IK_NAME = "/pr2_right_arm_kinematics/get_ik";
 typedef actionlib::SimpleActionClient<pr2_controllers_msgs::JointTrajectoryAction> TrajClient;
 
 class IKTrajectoryExecutor{
+private:
+    ros::NodeHandle node;
+    ros::ServiceClient ik_client;
+    ros::ServiceServer service;
+    pr2_controllers_msgs::JointTrajectoryGoal goal;
+    kinematics_msgs::GetPositionIK::Request ik_request;
+    kinematics_msgs::GetPositionIK::Response ik_response;
+    TrajClient *action_client;
 
+public:
+    IKTrajectoryExecutor(){
+
+        // create a client function for the IK service
+        ik_client = node.serviceClient<kinematics_msgs::GetPositionIK>(ARM_IK_NAME, true);
+
+        // wait for the various services to be ready
+        ROS_INFO("waiting for services to be ready");
+        ros::service::waitForService(ARM_IK_NAME);
+        ROS_INFO("Services ready");
+
+        // tell the joint trajectory action client that we want
+        // to spin a thread by default
+        action_client = new TrajClient("r_arm_controller/joint_trajectory_action", true);
+
+        // wait for the action server to come up
+        while(ros::ok() && !action_client->waitForServer(ros::Duration(5.0))){
+            ROS_INFO("Waiting for the joint_trajectory_action action server to come up");
+        }
+    }
 };
 
 int main(int argc, char** argv){
